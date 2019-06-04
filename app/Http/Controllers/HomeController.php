760693448e81525e031bff2270431851c3b1614e
rumd3x <edmurcardoso@gmail.com>
2019-06-03 21:05:55 -0300

    Root route handling
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -16,12 +17,23 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    public function index()
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            return redirect('login');
+        }
+
+        return redirect('home');
+    }
+
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function dashboard()
     {
         return view('home');
     }
