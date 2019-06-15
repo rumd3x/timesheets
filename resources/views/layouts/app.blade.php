92f1e59ea09fe25410daa02012ee596b0e609bf1
rumd3x <edmurcardoso@gmail.com>
2019-06-14 22:28:09 -0300

    Added auth protection for menu items
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,9 +33,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('timestamp.months') }}">Timestamps</a>
-                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('timestamp.months') }}">Timestamps</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
