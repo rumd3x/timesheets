eef607549c588951159947e35c28da0ebe3a0e2b
rumd3x <edmurcardoso@gmail.com>
2019-06-19 00:09:47 -0300

    Different visualization for input file
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -6,15 +6,33 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">App Settings</div>
-
                 <div class="card-body">
                     <form action="{{ route('app.settings') }}" method="POST">
+
                         @foreach ($inputs as $in)
+
+                            @if ($in['type'] == 'file')
+                                <div class="custom-file mb-4">
+                                    <label class="custom-file-label" for="{{ $in['name'] }}">{{ $in['display'] }}</label>
+                                    <input type="file" class="custom-file-input" id="{{ $in['name'] }}">
+                                </div>
+
+                                <script>
+                                    $("#{{ $in['name'] }}").on("change", function() {
+                                        var fileName = $(this).val().split("\\").pop();
+                                        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+                                    });
+                                </script>
+                                @continue
+                            @endif
+
                             <div class="form-group">
                                 <label for="{{ $in['name'] }}">{{ $in['display'] }}</label>
-                                <input class="form-control" type="{{ $in['type'] }}" name="{{ $in['name'] }}">
+                                <input class="form-control" type="{{ $in['type'] }}" name="{{ $in['name'] }}" id="{{ $in['name'] }}">
                             </div>
+
                         @endforeach
+
                         @csrf
                         <button class="btn btn-primary" type="submit">Save</button>
                     </form>
