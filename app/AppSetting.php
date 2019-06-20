9fb423834727b5d05836a83d103439d18cd72777
rumd3x <edmurcardoso@gmail.com>
2019-06-20 18:50:22 -0300

    Added constants for app settings names on AppSetting
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- app/AppSetting.php
+++ app/AppSetting.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AppSetting extends Model
 {
+    const SPREADSHEET_CURRENT_TEMPLATE_FILENAME = 'spreadsheet_current_template_filename';
+    const SPREADSHEET_INITIAL_COLUMN = 'spreadsheet_initial_column';
+    const SPREADSHEET_INITIAL_ROW = 'spreadsheet_initial_row';
+    const SPREADSHEET_GENERATION_EMAILS_REAL_RECIPIENTS = 'spreadsheet_generation_target_recipients';
+    const SPREADSHEET_GENERATION_EMAILS_TARGET_RECIPIENTS = 'spreadsheet_generation_real_recipients';
+    const SPREADSHEET_GENERATION_TARGET_HOURS = 'spreadsheet_generation_target_hours';
+
     protected $fillable = [
         'name',
         'value',
