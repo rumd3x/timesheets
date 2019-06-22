cbfcf256d759ad43749db6b08b055ec8e2248c55
rumd3x <edmurcardoso@gmail.com>
2019-06-22 15:09:01 -0300

    GenerateSpreadsheet command basic structure
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- app/Console/Commands/GenerateSpreadsheet.php
+++ app/Console/Commands/GenerateSpreadsheet.php
@@ -3,7 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\GoogleClient;
+use Carbon\Carbon;
+use App\Exceptions\InvalidJobArgumentException;
 
 class GenerateSpreadsheet extends Command
 {
@@ -12,7 +13,9 @@ class GenerateSpreadsheet extends Command
      *
      * @var string
      */
-    protected $signature = 'timesheet:generate';
+    protected $signature = 'timesheet:generate
+                            {month? : The month to generate the timesheet, defaults to last month}
+                            {year? : The year to generate the timesheet, default to current year}';
 
     /**
      * The console command description.
@@ -38,7 +41,25 @@ class GenerateSpreadsheet extends Command
      */
     public function handle()
     {
-        $client = GoogleClient::getClient();
-        $this->info('done');
+        $generationDate = Carbon::now()->subMonth()->day(1);
+
+        if (is_numeric($this->argument('year')) && ($this->argument('year') <= 1000 || $this->argument('year') >= 3000)) {
+            throw new InvalidJobArgumentException(sprintf('"%d" is not a valid year.', $this->argument('year')));
+        }
+
+        if (is_numeric($this->argument('month')) && ($this->argument('month') <= 0 || $this->argument('month') >= 13)) {
+            throw new InvalidJobArgumentException(sprintf('"%d" is not a valid month.', $this->argument('month')));
+        }
+
+        if ($this->argument('month') !== null && $this->argument('year') === null) {
+            $currentYear = Carbon::now()->format('Y');
+            $generationDate = Carbon::parse(sprintf('%d-%d-01', $currentYear, $this->argument('month')));
+        }
+
+        if ($this->argument('year') !== null) {
+            $generationDate = Carbon::parse(sprintf('%d-%d-01', $this->argument('year'), $this->argument('month')));
+        }
+
+        dump($generationDate->format('Y-m-d'));
     }
 }
