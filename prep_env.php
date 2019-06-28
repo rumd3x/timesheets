2c57692c8cd64410b4b5b5cdb6110e73ef7fe460
rumd3x <edmurcardoso@gmail.com>
2019-06-27 21:20:14 -0300

    Fix db symlink
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- prep_env.php
+++ prep_env.php
@@ -3,5 +3,5 @@
 if (!file_exists('.env')) {
     copy('.env.example', 'storage/.env') or exit(1);
     symlink('storage/.env', '.env') or exit(2);
-    symlink('storage/db.sqlite', 'database/db.sqlite') or exit(3);
+    symlink('storage/db.sqlite', '../database/db.sqlite') or exit(3);
 }
