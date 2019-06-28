129f8def0fdda5691fea3f5b8c64289a5aa25b3c
rumd3x <edmurcardoso@gmail.com>
2019-06-27 21:43:58 -0300

    Fix sqlite db env prepare
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- prep_env.php
+++ prep_env.php
@@ -3,5 +3,10 @@
 if (!file_exists('.env')) {
     copy('.env.example', 'storage/.env') or exit(1);
     symlink('storage/.env', '.env') or exit(2);
-    symlink('storage/db.sqlite', '../database/db.sqlite') or exit(3);
+}
+
+if (!file_exists('database/db.sqlite')) {
+    touch('storage/db.sqlite') or exit(4);
+    chdir('database') or exit(5);
+    symlink('../storage/db.sqlite', 'db.sqlite') or exit(3);
 }
